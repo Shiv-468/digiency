ad3d4681d961828f53b5c2ce00fedbe414d2d20c
Shiv-468 <shivmuwal.147@gmail.com>
2023-11-16 12:31:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+$('.slick_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    arrow:true,
+    autoplay: false,
+    prevArrow: ".prev-btn",
+    nextArrow:".next-btn",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 986,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 985,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
